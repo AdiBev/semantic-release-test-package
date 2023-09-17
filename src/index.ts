f64af1e22a588f7203eb3ef7bf883303bf99f914
Aditya Bevoor <adityabev@gmail.com>
2023-09-17 20:13:00 +0100

    BREAKING CHANGE: test breaking change

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,4 @@ const saySomething = (param: GreetingType) => {
   console.log(`${param} World!`)
 }
 
-const addAnother = () => console.log("hello")
-
-const addAnother1 = () => console.log("hello")
-
-export { saySomething, addAnother, addAnother1 }
+export { saySomething }
